3832f5a39b4c631ac7ee396b6fef98b3545552f7
Robert-29 <robert292005@gmail.com>
2026-04-03 15:39:56 -0600

    Se integran variables de entorno y json para backend

--- src/database/conexion.js
+++ src/database/conexion.js
@@ -1,11 +1,12 @@
 import { createPool } from "mysql2/promise";
+import 'dotenv/config'
 
 const pool = createPool({
-    host: 'junction.proxy.rlwy.net',
-    user: 'root',
-    password: 'XKtneuXwGpArPrcJVjGqJOVJNVJYAmeh',
-    port: 10311,
-    database: 'crystal_cove'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 });
 
 (async () => {
